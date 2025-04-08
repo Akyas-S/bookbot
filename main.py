f044811c2bdc40cb7fab1511b7c9db9aa3927386
Akyas-S <akyas.s.official@gmail.com>
2025-04-09 01:25:37 +0800

    added character counter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #imports
 from stats import find_num_words
+from stats import char_count
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,11 +9,15 @@ def get_book_text(path):
     
 def main():
     path = "/home/eksudee/workspaces/github.com/Akyas-S/bookbot/books/frankenstein.txt"
-
+    # reads the book
     contents = get_book_text(path)
+
     num_words = find_num_words(contents)
+    num_char = char_count(contents)
 
     print(f"{num_words} words found in the document")
+    print (num_char)
+    
 
 main()
 
--- stats.py
+++ stats.py
@@ -5,3 +5,15 @@ def find_num_words(contents):
         counter += 1   
 
     return counter
+
+def char_count(contents):
+    lwr_case_cntnt = contents.lower()
+    char_dict = {}
+    counter = 0
+    for char in lwr_case_cntnt:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    #print (char_dict)   
+    return char_dict
