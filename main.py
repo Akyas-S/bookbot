3e00f9fdab532136b529a6570eecd7d90a5b5736
Akyas-S <akyas.s.official@gmail.com>
2025-04-09 00:38:01 +0800

    refactor, added stat.py

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
+#imports
+from stats import find_num_words
+
 def get_book_text(path):
     with open(path) as f:
         contents = f.read()
         return contents
     
-def find_words(contents):    
-    words = contents.split()
-    counter = 0
-    for word in words:
-        counter += 1   
-
-    return counter
-
-
 def main():
     path = "/home/eksudee/workspaces/github.com/Akyas-S/bookbot/books/frankenstein.txt"
 
     contents = get_book_text(path)
-    num_words = find_words(contents)
+    num_words = find_num_words(contents)
 
     print(f"{num_words} words found in the document")
-
-
 
 main()
 
---
+++ stats.py
@@ -0,0 +1,7 @@
+def find_num_words(contents):    
+    words = contents.split()
+    counter = 0
+    for word in words:
+        counter += 1   
+
+    return counter
